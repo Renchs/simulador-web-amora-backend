22ea59f9d5554481f186c94f2d89876d16c0f2b6
Renchs <mathfm.dev@gmail.com>
2025-05-23 15:32:13 -0300

    fix: cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+
 import { validatePropertyPurchaseInput } from "./middlewares/validatePropertyPurchaseInput";
 import { simulatePropertyPurchaseHandle } from "./controllers/simulatePropertyController";
 
@@ -7,8 +9,10 @@ import { simulatePropertyPurchaseHandle } from "./controllers/simulatePropertyCo
 
 
 dotenv.config();
+
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 app.post("/simulacao", validatePropertyPurchaseInput, simulatePropertyPurchaseHandle)
